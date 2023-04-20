6450f318655d39032d1db5389043cc3d7ef40ee5
Damien Switzer <damienswitzer@hotmail.com>
2023-04-20 08:54:18 -0700

    Design MovieCards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,5 +1,16 @@
 import "./MovieCard.scss";
+import placeholderImage from "../../images/placeholder.jpg";
 
-export default function MovieCard() {
-  return <article className="movie-card">Movie Card</article>;
+export default function MovieCard({ movie }) {
+  const { id, title, release_date, poster_path, vote_average } = movie;
+  return (
+    <article className="movie-card">
+      <img className="movie-card__thumbnail" src={placeholderImage} alt="thumbnail" />
+      <div className="movie-card__content">
+        <h4 className="movie-card__title">{title}</h4>
+        <p className="movie-card__text">{release_date}</p>
+        <p className="movie-card__text">{vote_average}</p>
+      </div>
+    </article>
+  );
 }
