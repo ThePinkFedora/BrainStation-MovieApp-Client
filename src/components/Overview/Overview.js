7f39d992be651f2deb38e4147747d50dccf14e74
bleykam <bleykam@gmail.com>
2023-04-20 12:44:53 -0400

    styled overview page

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,17 +1,23 @@
 import {useParams, useState} from 'react';
+import placeholderImage from "../../images/placeholder.jpg";
 import "./Overview.scss";
-
+import Details from '../../data/details.json'
 export default function Overview(){
+const {title, runtime, release_date, poster_path, genres, overview, vote_average}=Details;
+
     return(
         <main>
-        <h1>{title}</h1>
+        <h1 className = "overview__title">{title}</h1>
         <div>
-            <div>
-            <div>{year}</div>
-            <div>{rating}</div>
-            <div>{length}</div>
+            <div className = "overview__details">
+            
+            <div className = "overview__date">{release_date}</div>
+            <div className="overview__vote">{vote_average}</div>
+            <div className="overview__runtime">{runtime}</div>
+            <div className="overview__genre">{genres[0].name}</div>
             </div>
-            <div>{picture}</div>
+            <div className="overview__overview">{overview}</div>
+           <div className="overview__image-div"><img className="overview__image" src={placeholderImage} alt="thumbnail" /></div> 
         </div>
         </main>
     )
