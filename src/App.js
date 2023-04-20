91299d3e54c959e18ec0ad07a114279cded418f5
Damien Switzer <damienswitzer@hotmail.com>
2023-04-20 12:45:13 -0700

    Implement watchlist api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
@@ -9,38 +9,62 @@ import WatchList from "./components/WatchList/WatchList";
 import axios from "axios";
 
 function App() {
-
-  const [movieList, setMovieList]=useState(null);
+  const [movieList, setMovieList] = useState(null);
   const [watchhlist, setWatchlist] = useState([]);
-  useEffect(()=>{
-    axios.get('https://api.themoviedb.org/3/discover/movie?api_key=18738ecfafb89b9dc502473373a38d50')
-    .then((res)=>{
-    setMovieList(res.data.results)
-    })
+
+  useEffect(() => {
+    axios.get("https://api.themoviedb.org/3/discover/movie?api_key=18738ecfafb89b9dc502473373a38d50").then((res) => {
+      setMovieList(res.data.results);
+    });
   }, []);
 
-  if(!movieList){
-    return <div>Loading...</div>
+  ///GET localhost:8080/watchlist
+  //Returns the watchlist
+
+  //POST localhost:8080/watchlist
+  //Adds an item to the watchlist
+  //Returns the watchlist
+
+  //DELETE localhost:8080/watchlist/:id
+  //Deletes an item from the watchlist
+  //Returns the watchlist
+
+  //Download watchlist from API
+  useEffect(() => {
+    axios.get("http://localhost:8080/watchlist/").then((res) => setWatchlist(res.data));
+  }, []);
+
+  if (!movieList) {
+    return <div>Loading...</div>;
   }
- 
 
   const handleAddToWatchlist = (movieId) => {
-    setWatchlist([...watchhlist, movieId]);
+    //POST id to watchlist on API
+    axios.post("http://localhost:8080/watchlist/", { id: movieId }).then((res) => {
+      setWatchlist(res.data);
+    });
+
+    //Update watchlist state
+    // setWatchlist([...watchhlist, movieId]);
   };
   const handleRemoveFromWatchlist = (movieId) => {
-    setWatchlist(watchhlist.filter((id) => id !== movieId));
+    // setWatchlist(watchhlist.filter((id) => id !== movieId));
+    //DELETE id from watchlist on API
+    axios.delete(`http://localhost:8080/watchlist/${movieId}`).then((res) => {
+      setWatchlist(res.data);
+    });
   };
 
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home movieList={movieList}/>} />
+        <Route path="/" element={<Home movieList={movieList} />} />
         <Route
           path="/:id"
           element={<Overview onAddToWatchlist={handleAddToWatchlist} onRemoveFromWatchlist={handleRemoveFromWatchlist} watchlist={watchhlist} />}
         />
-        <Route path="/watchlist" element ={<WatchList movieList = {movieList} watchlist={watchhlist}/>} />
+        <Route path="/watchlist" element={<WatchList movieList={movieList} watchlist={watchhlist} />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,10 @@ export default function NavBar() {
   return (
     <header className="nav-bar">
       <NavLink to="/">
-        <h2 class="nav-bar__logo">MOVIE APP</h2>
+        <h2 className="nav-bar__logo">MOVIE APP</h2>
       </NavLink>
       <NavLink to="/watchlist">
-        <h2 class="nav-bar__link">Watchlist</h2>
+        <h2 className="nav-bar__link">Watchlist</h2>
       </NavLink>
     </header>
   );
