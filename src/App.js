106ebdbe248182f3e93d7c77a932e7b3aa4c1136
Damien Switzer <damienswitzer@hotmail.com>
2023-04-20 12:55:39 -0700

    Add watchlist counter

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavBar />
+      <NavBar watchlistCount={watchhlist.length} />
       <Routes>
         <Route path="/" element={<Home movieList={movieList} />} />
         <Route
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,14 @@
 import "./NavBar.scss";
 import { NavLink } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ watchlistCount }) {
   return (
     <header className="nav-bar">
       <NavLink to="/">
         <h2 className="nav-bar__logo">MOVIE APP</h2>
       </NavLink>
       <NavLink to="/watchlist">
-        <h2 className="nav-bar__link">Watchlist</h2>
+        <h2 className="nav-bar__link">Watchlist ({watchlistCount})</h2>
       </NavLink>
     </header>
   );
